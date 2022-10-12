603b5554091b69fec437656d5ad17ded0134ae57
Zach Schwartz <zachatorcodes@gmail.com>
2022-10-12 14:43:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  let rideGreeting;
+
+  if (rideDistance <= 400) {
+    rideGreeting = `This one is on me!`;
+  }
+  else if ((rideDistance > 400) && (rideDistance <= 2000)) {
+    rideGreeting = `That will be twenty bucks.`;
+  }
+  else if ((rideDistance > 2000) && (rideDistance <= 2500)) {
+    rideGreeting = `I will gladly take your thirty bucks.`;
+  }
+  else {
+    rideGreeting = `No can do.`;
+  }
+  return rideGreeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return ((city === "NYC") || (city === "New York City")) ? ("Ok, sounds good.") : (`No go.`);
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
